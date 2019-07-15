cb20a53752ede1b2e26f66cd92703336948201fe
Alexander Riccio <test35965+learn@gmail.com>
2019-07-15 16:38:06 -0400

    solve lab

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,20 @@ const codes = [
   "a"
 ];
 
+let lastKonamiIndex = 0;
+
 function init() {
   // your code here
+  document.body.addEventListener('keydown', function(event) {
+    if (event.key === codes[lastKonamiIndex]) {
+      lastKonamiIndex++;
+      if (lastKonamiIndex === codes.length) {
+        alert('you cool, my mofo');
+        lastKonamiIndex = 0;
+      }
+    }
+    else {
+      lastKonamiIndex = 0;
+    }
+  });
 }
